314302840bc12582148c7a424ded684d330a7e08
debrawu <dwu1124@gmail.com>
2020-11-09 19:37:38 -0600

    level 2 i guess

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,60 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select('tbody');
+
+// console log the ufo data from data.js
+console.log(tableData);
+
+// loop through the data and console log each ufo sighting object
+tableData.forEach(ufoData => {
+    var row = tbody.append('tr');
+    Object.values(ufoData).forEach(value => {
+        console.log(value);
+        row.append('td').text(value);
+    })
+})
+
+// EVENT LISTENERS
+
+// select the button for users to submit
+var button = d3.select('#filter-btn');
+
+// complete click handler for the form
+button.on('click', function(){
+    // select input element and get the HTML node for the DATE
+    var inputElement = d3.select('#datetime');
+
+    // get value property of the input element
+    var inputValue = inputElement.property('value');
+
+    // use form input to filter data by date or city or state or country or shape
+    var dataFilter = tableData.filter(data => data.datetime === inputValue ||
+                                              data.city.toLowerCase() === inputValue ||
+                                              data.state.toLowerCase() === inputValue ||
+                                              data.country.toLowerCase() === inputValue ||
+                                              data.shape.toLowerCase() === inputValue);
+
+    console.log(dataFilter);
+
+
+    // print out the information in the empty table
+
+    // create an empty table within the html
+    tbody.html('');
+    
+    // display the filtered data
+    dataFilter.forEach((sightings) => {
+        console.log(sightings);
+
+        // append on row for each sighting
+        var row = tbody.append('tr');
+
+        Object.entries(sightings).forEach(function([key, value]) {
+            console.log(key,value);
+            var item = row.append('td');
+            item.text(value);
+        });
+    });
+});
